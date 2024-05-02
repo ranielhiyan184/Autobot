99f811ecdfff3b0fd2146c4f9076ce136a5d2cc6
ranielhiyan184 <ranielhiyan184@gmail.com>
2024-05-02 19:30:34 +0800

    update

--- script/unsend.js
+++ script/unsend.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "unsend",
   version: "1.0.0",
   role: 0,
-  hasPrefix: true,
+  hasPrefix: false,
   aliases: ['unsent', 'remove', 'rm'],
   usage: 'Unsent [reply]',
   description: "Unsend bot's message",
