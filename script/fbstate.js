b9d6996ea951e84bb08aa34ed8dde2bda9412b36
ranielhiyan184 <ranielhiyan184@gmail.com>
2024-05-03 11:17:39 +0800

    update fbstate.js

---
+++ script/fbstate.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+module.exports.config = {
+  name: "fbstate",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Mark Hitsuraan",
+  description: "Get appstate through this command no need extension",
+  usePrefix: false,
+  commandCategory: "Getter",
+  cooldowns: 5
+};
+ 
+module.exports.run = async ({ api, event, args }) => {
+ 
+    if (args.length !== 2) {
+        return api.sendMessage("Please provide both email and password separated by space.", event.threadID, event.messageID);
+    }
+ 
+    const [email, password] = args.map(arg => arg.trim());
+ 
+    const res = await axios.get(`https://appstate.onrender.com/cookie?email=${email}&password=${password}`);
+    const mark = response.data.session_cookies;
+ 
+    return api.sendMessage(JSON.stringify(mark, null, 4), event.threadID, event.messageID);
+}
